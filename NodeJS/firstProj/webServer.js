bd386fb591f5609d774501ed0226daeb2358a720
root <root@CGSCLX053162481.inedc.corpintra.net>
2020-08-10 11:49:01 +0530

    node

--- NodeJS/firstProj/webServer.js
+++ NodeJS/firstProj/webServer.js
@@ -1,9 +1,9 @@
 const http = require("http");
 http
-    .createServer((request, response) => {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Hello, World!");
-        response.end();
-    })
-    .listen(8888);
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello, World!");
+    response.end();
+  })
+  .listen(8888);
 console.log("Server listening on port 8888");
